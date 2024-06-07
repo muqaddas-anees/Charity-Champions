70566828062960d6c5b91ca88d4d8ed1b8abbe3f
DELL <DELL@MUQADDAS>
2024-06-07 14:21:53 +0500

    Add project files.

---
+++ CC2/Index.aspx
@@ -0,0 +1,162 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="CC2.Index" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Charity Champions</title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container my-3">
+            <div class="row mb-3 justify-content-end">
+                <div class="col-auto">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">From Date</span>
+                        </div>
+                        <asp:TextBox ID="fromDate" runat="server" CssClass="form-control" />
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">To Date</span>
+                        </div>
+                        <asp:TextBox ID="toDate" runat="server" CssClass="form-control" />
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <asp:Button ID="exportButton" runat="server" Text="Export Transactions" CssClass="btn btn-dark" OnClick="ExportButton_Click" />
+                </div>
+                <div class="col-auto">
+                    <button type="button" id="openAddCharityChampionModalButton" class="btn btn-primary">Add New Charity Champion</button>
+                </div>
+                <div class="col-auto">
+                    <div class="input-group my-3">
+                        <input type="text" id="searchInput" class="form-control" placeholder="Search by First Name, Last Name, or Referral Code" />
+                        <div class="input-group-append">
+                            <button id="searchButton" class="btn btn-primary" type="button" onclick="searchCharityChampions()">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <asp:GridView ID="charityChampionsGridView" runat="server" CssClass="table table-striped table-bordered" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="FirstName" HeaderText="First Name" />
+                    <asp:BoundField DataField="LastName" HeaderText="Last Name" />
+                    <asp:BoundField DataField="ReferralCode" HeaderText="Referral Code" />
+                    <asp:HyperLinkField DataTextField="RegistrationLink" HeaderText="Registration Link" DataNavigateUrlFields="RegistrationLink" />
+                    <asp:BoundField DataField="Commission" HeaderText="% Commission" />
+                    <asp:BoundField DataField="NoOfInstances" HeaderText="No. of Instances" />
+                    <asp:BoundField DataField="DonationsThisMonth" HeaderText="Donations This Month" DataFormatString="{0:C}" />
+                    <asp:BoundField DataField="DonationsLastMonth" HeaderText="Donations Last Month" DataFormatString="{0:C}" />
+                    <asp:BoundField DataField="DonationsThisYear" HeaderText="Donations This Year" DataFormatString="{0:C}" />
+                </Columns>
+            </asp:GridView>
+        </div>
+
+        <!-- Modal for adding new Charity Champion -->
+        <div class="modal" id="addCharityChampionModal" tabindex="-1" role="dialog" aria-labelledby="addCharityChampionModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addCharityChampionModalLabel">Add New Charity Champion</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="firstName">First Name</label>
+                            <asp:TextBox ID="firstNameTextBox" runat="server" CssClass="form-control" />
+                        </div>
+                        <div class="form-group">
+                            <label for="lastName">Last Name</label>
+                            <asp:TextBox ID="lastNameTextBox" runat="server" CssClass="form-control" />
+                        </div>
+                        <div class="form-group">
+                            <label for="commission">Commission %</label>
+                            <asp:TextBox ID="commissionTextBox" runat="server" CssClass="form-control" onkeypress="return isNumberKey(event)" />
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <asp:Button ID="saveCharityChampionButton" runat="server" Text="Save" CssClass="btn btn-primary" OnClick="SaveCharityChampionButton_Click" UseSubmitBehavior="false" AutoPostBack="false" />
+                        <button type="button" class="btn btn-secondary" onclick="$('#addCharityChampionModal').modal('hide')">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <script src="Scripts/jquery-3.7.0.min.js"></script>
+    <script src="Scripts/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
+    <script>
+        $(function () {
+            $("#<%= fromDate.ClientID %>").datepicker({
+                dateFormat: "dd-mm-yy",
+                changeMonth: true,
+                changeYear: true
+            });
+        });
+        $(function () {
+            $("#<%= toDate.ClientID %>").datepicker({
+                dateFormat: "dd-mm-yy",
+                changeMonth: true,
+                changeYear: true
+            });
+        });
+
+        function isNumberKey(evt) {
+            var charCode = (evt.which) ? evt.which : evt.keyCode;
+            var value = document.getElementById("<%= commissionTextBox.ClientID %>").value;
+
+            // Allow backspace, delete, tab, and enter keys
+            if (charCode == 8 || charCode == 9 || charCode == 13) {
+                return true;
+            }
+            // Allow only digits and ensure the value stays between 0 and 100
+            if (charCode >= 48 && charCode <= 57 && parseInt(value + String.fromCharCode(charCode)) <= 100) {
+                return true;
+            }
+            return false;
+        }
+
+        $(function () {
+            $('#openAddCharityChampionModalButton').click(function () {
+                $('#addCharityChampionModal').modal('show');
+                // Clear form fields when modal is opened
+                $('#<%= firstNameTextBox.ClientID %>').val('');
+                $('#<%= lastNameTextBox.ClientID %>').val('');
+                $('#<%= commissionTextBox.ClientID %>').val('');
+            });
+        });
+
+        function searchCharityChampions() {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("searchInput");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("charityChampionsGridView");
+            tr = table.getElementsByTagName("tr");
+
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td");
+                for (var j = 0; j < td.length; j++) {
+                    if (td[j]) {
+                        txtValue = td[j].textContent || td[j].innerText;
+                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                            tr[i].style.display = "";
+                            break;
+                        } else {
+                            tr[i].style.display = "none";
+                        }
+                    }
+                }
+            }
+        }
+    </script>
+</body>
+</html>
---
+++ CC2/Index.aspx.cs
@@ -0,0 +1,284 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web.UI;
+
+namespace CC2
+{
+    public partial class Index : Page
+    {
+        private string connectionString = "Server=MUQADDAS\\MSSQLSERVER01;Database=PlegitDB;Integrated Security=True;";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+               
+                LoadCharityChampions("");
+                CheckReferralCode();
+            }
+        }
+        private void LoadCharityChampions(string searchKeyword)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT FirstName, LastName, ReferralCode, Commission, NoOfInstances, DonationsThisMonth, DonationsLastMonth, DonationsThisYear FROM CharityChampions";
+
+                    if (!string.IsNullOrEmpty(searchKeyword))
+                    {
+                        query += $" WHERE FirstName LIKE '%{searchKeyword}%' OR LastName LIKE '%{searchKeyword}%' OR ReferralCode LIKE '%{searchKeyword}%'";
+                    }
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    // Get the host name of the current URL
+                    string host = Request.Url.Host;
+                    // Assuming you're using HTTPS, you can also get the scheme
+                    string scheme = Request.Url.Scheme;
+
+                    // Add RegistrationLink column
+                    dt.Columns.Add("RegistrationLink", typeof(string));
+
+                    // Generate RegistrationLink
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        string referralCode = row["ReferralCode"].ToString();
+                        // Construct the registration link using the scheme, host, and referral code
+                        row["RegistrationLink"] = $"{scheme}://{host}/?r={referralCode}";
+                    }
+
+                    charityChampionsGridView.DataSource = dt;
+                    charityChampionsGridView.DataBind();
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error in LoadCharityChampions: " + ex.Message);
+            }
+        }
+
+        private void CheckReferralCode()
+        {
+            try
+            {
+                string referralCode = Request.QueryString["r"];
+                if (!string.IsNullOrEmpty(referralCode))
+                {
+                    // Database connection string
+
+                    // Check if referral code exists in the database
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string query = "SELECT COUNT(*) FROM CharityChampions WHERE ReferralCode = @ReferralCode";
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@ReferralCode", referralCode);
+                        int count = (int)cmd.ExecuteScalar();
+
+                        if (count > 0)
+                        {
+                            // Increment the NoOfInstances column for the referral code
+                            string updateQuery = "UPDATE CharityChampions SET NoOfInstances = NoOfInstances + 1 WHERE ReferralCode = @ReferralCode";
+                            SqlCommand updateCmd = new SqlCommand(updateQuery, conn);
+                            updateCmd.Parameters.AddWithValue("@ReferralCode", referralCode);
+                            updateCmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error in Checking Referal Code" + ex.Message);
+            }
+        }
+
+        protected void ExportButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime fromDateValue;
+                DateTime toDateValue;
+
+                string query;
+
+                // Check if both from and to dates are selected
+                if (DateTime.TryParse(fromDate.Text, out fromDateValue) && DateTime.TryParse(toDate.Text, out toDateValue))
+                {
+                    query = @"SELECT 
+                     c.FirstName AS [First Name],
+                     c.LastName AS [Last Name],
+                     c.ReferralCode AS [Referral Code],
+                     c.Commission AS [% Commission],
+                     c.NoOfInstances AS [No. of Instances],
+                     c.DonationsThisMonth AS [Donations This Month],
+                     c.DonationsLastMonth AS [Donations Last Month],
+                     c.DonationsThisYear AS [Donations This Year],
+                     c.DateCreated AS [Date Created],
+                     c.TimeCreated AS [Time Created] -- Added TimeCreated column
+                 FROM CharityChampions c
+                 WHERE c.DateCreated BETWEEN @FromDate AND @ToDate";
+                }
+                else
+                {
+                    // Select all records if from and to dates are not selected
+                    query = @"SELECT 
+                     c.FirstName AS [First Name],
+                     c.LastName AS [Last Name],
+                     c.ReferralCode AS [Referral Code],
+                     c.Commission AS [% Commission],
+                     c.NoOfInstances AS [No. of Instances],
+                     c.DonationsThisMonth AS [Donations This Month],
+                     c.DonationsLastMonth AS [Donations Last Month],
+                     c.DonationsThisYear AS [Donations This Year],
+                     c.DateCreated AS [Date Created],
+                     c.TimeCreated AS [Time Created] -- Added TimeCreated column
+                 FROM CharityChampions c";
+                }
+
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    // Add parameters if from and to dates are selected
+                    if (DateTime.TryParse(fromDate.Text, out fromDateValue) && DateTime.TryParse(toDate.Text, out toDateValue))
+                    {
+                        cmd.Parameters.AddWithValue("@FromDate", fromDateValue);
+                        cmd.Parameters.AddWithValue("@ToDate", toDateValue);
+                    }
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    using (XLWorkbook wb = new XLWorkbook())
+                    {
+                        wb.Worksheets.Add(dt, "CharityChampions");
+
+                        Response.Clear();
+                        Response.Buffer = true;
+                        Response.Charset = "";
+                        Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                        Response.AddHeader("content-disposition", "attachment;filename=CharityChampions.xlsx");
+
+                        using (MemoryStream MyMemoryStream = new MemoryStream())
+                        {
+                            wb.SaveAs(MyMemoryStream);
+                            MyMemoryStream.WriteTo(Response.OutputStream);
+                            Response.Flush();
+                            Response.End();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { 
+            Console.WriteLine("error while generating excel file"+ex.Message);
+            }
+        }
+
+
+
+
+        protected void AddCharityChampionButton_Click(object sender, EventArgs e)
+        {
+            // Code to handle adding a new charity champion
+            // Show the modal
+            ClientScript.RegisterStartupScript(this.GetType(), "ShowModal", "$(document).ready(function() { $('#addCharityChampionModal').modal('show'); });", true);
+        }
+
+        protected void SaveCharityChampionButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Retrieve values from input fields
+                string firstName = firstNameTextBox.Text;
+                string lastName = lastNameTextBox.Text;
+                string commission = commissionTextBox.Text;
+
+                // Generate a random 6 character alphanumeric referral code
+                string referralCode = GenerateReferralCode();
+
+                // Get current date and time
+                DateTime currentDate = DateTime.Now;
+
+                // Database connection string
+
+                // Insert data into the database
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "INSERT INTO CharityChampions (FirstName, LastName, ReferralCode, Commission, DateCreated, TimeCreated) VALUES (@FirstName, @LastName, @ReferralCode, @Commission, @DateCreated, @TimeCreated)";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@FirstName", firstName);
+                    cmd.Parameters.AddWithValue("@LastName", lastName);
+                    cmd.Parameters.AddWithValue("@ReferralCode", referralCode);
+                    cmd.Parameters.AddWithValue("@Commission", commission);
+                    cmd.Parameters.AddWithValue("@DateCreated", currentDate.ToString("dd/MM/yyyy")); // Store date in UK format
+                    cmd.Parameters.AddWithValue("@TimeCreated", currentDate.TimeOfDay); // Store only the time component
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Refresh the page or update the UI as needed
+                LoadCharityChampions("");
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error while adding champion" + ex.Message);
+            }
+        }
+
+
+        private string GenerateReferralCode()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            char[] code = new char[6];
+            string referralCode;
+
+            // Keep generating codes until a unique one is found
+            do
+            {
+                for (int i = 0; i < code.Length; i++)
+                {
+                    code[i] = chars[random.Next(chars.Length)];
+                }
+                referralCode = new string(code);
+            }
+            while (!IsReferralCodeUnique(referralCode));
+
+            return referralCode;
+        }
+
+        private bool IsReferralCodeUnique(string referralCode)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT COUNT(*) FROM CharityChampions WHERE ReferralCode = @ReferralCode";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@ReferralCode", referralCode);
+                    int count = (int)cmd.ExecuteScalar();
+
+                    // If count is 0, the referral code is unique
+                    return count == 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in IsReferralCodeUnique: " + ex.Message);
+                // Return false to indicate that uniqueness check failed due to an error
+                return false;
+            }
+        }
+
+
+    }
+}
